13941a9e090cf9c31b9cfeb54111b8df7385dc82
SaiVenkatDhulipudi <82017014+SaiVenkatDhulipudi@users.noreply.github.com>
2021-06-04 23:15:05 +0530

    Update and rename DSA-array to DSA-array.cpp

--- DSA-array.cpp
+++ DSA-array.cpp
@@ -15,8 +15,9 @@ int main() {
 	for(i=0;i<n;i++){
 	  cin>>a[i];
 	}
-	cout<<"after traversing"<<endl;
+	cout<<"after traversing"<<endl;//traversing
 	printk(a,n);
+		//inserting
 	int pos,num;
 	cin>>pos;
 	if(pos>n+1||pos<0){
@@ -30,6 +31,20 @@ int main() {
 	  a[pos-1]=num;
 	  n++;
 	}
-	cout<<"\nafter inserting" <<num<<"at:"<<pos<<endl;
+	cout<<"\nafter inserting" <<num<<" at: "<<pos<<endl;
+	printk(a,n);
+	//deleting
+	int pos1;
+	cin>>pos1;
+	if(pos<0||pos>n){
+	  cout<<"overflow"<<endl;
+	}
+	else{
+	  for(i=pos1-1;i<n;i++){
+	    a[i]=a[i+1];
+	  }
+	  n--;
+	}
+	cout<<"\nafter DELETING " <<a[pos1-1]<<" at: "<<pos1<<endl;
 	printk(a,n);
 }
