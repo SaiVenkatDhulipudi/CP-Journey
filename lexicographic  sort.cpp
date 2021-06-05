23a12fcc6e3e1b2b1617976c51078084cca72a76
SaiVenkatDhulipudi <82017014+SaiVenkatDhulipudi@users.noreply.github.com>
2021-06-05 22:25:10 +0530

    Create lexicographic  sort.cpp

---
+++ lexicographic  sort.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool comp(string &s1,string &s2)
+{
+    return s1.size() < s2.size();
+}
+
+int main() {
+ vector<string>arr;
+ string s;
+ getline(cin,s);
+ string s1;
+ istringstream s2(s);
+ while(s2>>s1){
+   arr.push_back(s1);
+ }
+ // sorting sentence lexicographically
+ sort(arr.begin(),arr.end(),comp);
+ for(auto j:arr){
+   cout<<j<<" ";
+ }
+}
+
+
