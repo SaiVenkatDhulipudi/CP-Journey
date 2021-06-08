38e2555bd084839febd6f9067b65cd248ec9f6f8
SaiVenkatDhulipudi <82017014+SaiVenkatDhulipudi@users.noreply.github.com>
2021-06-08 16:07:56 +0530

    Create quicksort.cpp

---
+++ quicksort.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+int partition(int A[], int low, int high)
+{
+    int pivot = A[low];
+    int i = low + 1;
+    int j = high;
+    int temp;
+
+    do
+    {
+        while (A[i] <= pivot)
+        {
+            i++;
+        }
+
+        while (A[j] > pivot)
+        {
+            j--;
+        }
+
+        if (i < j)
+        {
+            temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+    } while (i < j);
+
+
+    temp = A[low];
+    A[low] = A[j];
+    A[j] = temp;
+    return j;
+}
+
+void quickSort(int A[], int low, int high)
+{
+    int partitionIndex; // Index of pivot after partition
+
+    if (low < high)
+    {
+        partitionIndex = partition(A, low, high); 
+        quickSort(A, low, partitionIndex - 1);  // sort left subarray 
+        quickSort(A, partitionIndex + 1, high); // sort right subarray
+    }
+}
+int main() {
+int n;
+cin>>n;
+int a[n];
+for(int i=0;i<n;i++){
+  cin>>a[i];
+}
+quickSort(a,0,n-1);
+for(int i=0;i<n;i++){
+  cout<<a[i]<<" ";
+}
+}
