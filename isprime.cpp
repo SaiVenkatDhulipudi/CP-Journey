b9608d4217b94372dc5c685702a231daf807b5c5
SaiVenkatDhulipudi <82017014+SaiVenkatDhulipudi@users.noreply.github.com>
2021-07-10 19:12:51 +0530

    Update isprime.cpp

--- isprime.cpp
+++ isprime.cpp
@@ -6,17 +6,16 @@ bool prime(int n){
   if(n==1)return false;
   
  else if(n==2||n==3) return true;
+ else if(n%2==0||n%3==0)return false;
   else{
-  for(int i=5;i*i<=n;i+=6){
+  for(int i=5;i<=sqrt(n);i+=6){
     if(n%i==0||n%(i+2)==0){
       return false;
       break;
     }
-    else{
-      return true;
-    }
   }
   }
+    return true;
 }
 int main(){
   int n;cin>>n;
