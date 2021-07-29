260cf39830ea289dc768cc719a4b45a0caf651b5
SaiVenkatDhulipudi <82017014+SaiVenkatDhulipudi@users.noreply.github.com>
2021-07-29 20:54:52 +0530

    Create Detect_cycle_UG.cpp

---
+++ Graphs/Detect_cycle_UG.cpp
@@ -0,0 +1,58 @@
+/*INPUT:
+6
+0 1
+1 2
+1 3
+2 3
+2 4
+2 5
+OUTPUT:
+CYCLE DETECTED
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+map<int,bool>visited;
+map<int,list<int>>mp;
+bool dfsrec(int s,int parent){
+  visited[s]=true;
+  for(auto v:mp[s]){
+    if(visited[v]==false){
+      if(dfsrec(v,s)==true){
+        return true;
+      }
+    }
+    else if(v!=parent){
+      return true;
+    }
+  }
+  return false;
+}
+bool dfs(int n){
+  for(auto i:mp){
+    if(visited[i.first]==false){
+      if(dfsrec(i.first,-1)){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+void add_edge(int s,int d){
+  mp[s].push_back(d); 
+}
+int main() {
+ int n;
+ cin>>n;
+ int s,d;
+ for(int i=0;i<n;i++){
+   cin>>s>>d;
+   add_edge(s,d);
+ }
+if(dfs(n)){
+  cout<<"CYCLE DETECTED"<<endl;
+}
+else{
+  cout<<"NO CYCLE"<<endl;
+}
+}
